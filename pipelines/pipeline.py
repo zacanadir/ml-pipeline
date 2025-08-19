88d1c30b6d3993362d79842c3868a704b45f456a
Zakaria Nadir <zacanadir@gmail.com>
2025-08-19 15:14:44 -0400

    fixed pipeline script

--- pipelines/pipeline.py
+++ pipelines/pipeline.py
@@ -1,20 +1,27 @@
+
 from kfp import dsl
 import os
 
-IMAGE_URI = os.environ.get("PIPELINE_IMAGE_URI", "us-central1-docker.pkg.dev/$PROJECT_ID/myrepo/ml-pipeline:latest")
+# Make sure the environment variable is passed correctly
+IMAGE_URI = os.environ.get(
+    "PIPELINE_IMAGE_URI",
+    "us-central1-docker.pkg.dev/modified-wonder-468716-e8/myrepo/ml-pipeline:latest"
+)
 
 @dsl.component(base_image=IMAGE_URI)
 def train_op():
     import numpy as np
     import joblib
     from sklearn.linear_model import LinearRegression
+    import os
 
     X = np.array([[1], [2], [3], [4]])
     y = np.array([2, 4, 6, 8])
     model = LinearRegression().fit(X, y)
 
-    joblib.dump(model, "model.joblib")
-    print("✅ Model trained and saved.")
+    model_path = os.path.join(os.environ["AIP_PIPELINE_ROOT"], "model.joblib")
+    joblib.dump(model, model_path)
+    print(f"✅ Model trained and saved at {model_path}")
 
 @dsl.pipeline(name="hello-pipeline")
 def pipeline():
