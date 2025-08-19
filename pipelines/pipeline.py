f84966288b6e5a0c83cfdb85defad2cab965007f
Zakaria Nadir <zacanadir@gmail.com>
2025-08-19 19:26:20 -0400

    consistent model naming

--- pipelines/pipeline.py
+++ pipelines/pipeline.py
@@ -6,23 +6,21 @@ IMAGE_URI = os.environ.get(
     "PIPELINE_IMAGE_URI",
     "us-central1-docker.pkg.dev/modified-wonder-468716-e8/myrepo/ml-pipeline:latest"
 )
-
 @dsl.component(base_image=IMAGE_URI)
-def train_op(model_path: dsl.OutputPath(str)):
-    import numpy as np
-    import joblib
+def train_op(model_path: dsl.OutputPath(str), commit_id: str = "unknown"):
+    import numpy as np, joblib, os
     from sklearn.linear_model import LinearRegression
 
-    # Dummy training data
     X = np.array([[1], [2], [3], [4]])
     y = np.array([2, 4, 6, 8])
-
-    # Train model
     model = LinearRegression().fit(X, y)
 
-    # Save artifact to provided path (Vertex AI injects a GCS location)
-    joblib.dump(model, model_path)
-    print(f"✅ Model trained and saved at {model_path}")
+    filename = f"model_{commit_id}.joblib"
+    out_file = os.path.join(model_path, filename)
+    joblib.dump(model, out_file)
+
+    print(f"✅ Model saved to {out_file}")
+
 
 @dsl.pipeline(name="hello-pipeline")
 def pipeline():
