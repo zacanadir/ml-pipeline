f3963be42e390bbd20a90e0d77aa7c9e7d9bf90c
Zakaria Nadir <zacanadir@gmail.com>
2025-08-19 20:47:51 -0400

    fix model saving

--- pipelines/pipeline.py
+++ pipelines/pipeline.py
@@ -1,22 +1,27 @@
 from kfp import dsl
 import os
 
-# Use the image you built/pushed (passed from Cloud Build)
 IMAGE_URI = os.environ.get(
     "PIPELINE_IMAGE_URI",
     "us-central1-docker.pkg.dev/modified-wonder-468716-e8/myrepo/ml-pipeline:latest"
 )
+
 @dsl.component(base_image=IMAGE_URI)
 def train_op(model_path: dsl.OutputPath(str), commit_id: str = "unknown"):
-    import numpy as np, joblib, os
+    import numpy as np
+    import joblib
     from sklearn.linear_model import LinearRegression
+    import os
 
     X = np.array([[1], [2], [3], [4]])
     y = np.array([2, 4, 6, 8])
     model = LinearRegression().fit(X, y)
 
-    filename = f"model_{commit_id}.joblib"
-    out_file = os.path.join(model_path, filename)
+    # Ensure folder exists
+    os.makedirs(model_path, exist_ok=True)
+
+    # Full file path
+    out_file = os.path.join(model_path, f"model_{commit_id}.joblib")
     joblib.dump(model, out_file)
 
     print(f"✅ Model saved to {out_file}")
